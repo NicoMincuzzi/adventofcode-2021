345b69cc4f995f369c1dc509ccfd3db8336d3dd8
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-12-11 19:36:19 +0100

    Day 3: Binary Diagnostic-Part Two

--- src/main/kotlin/com/github/nicomincuzzi/Day3BinaryDiagnostic.kt
+++ src/main/kotlin/com/github/nicomincuzzi/Day3BinaryDiagnostic.kt
@@ -8,16 +8,55 @@ class Day3BinaryDiagnostic {
         var gammaRate = ""
         var epsilonRate = ""
         for (i in 0 until diagnosticReport[0].length) {
-            val tempDict = hashMapOf<Int, Int>()
-            for (elem in diagnosticReport) {
-                val bit = getNumericValue(elem.toCharArray()[i])
-                tempDict[bit] = if (tempDict.containsKey(bit)) tempDict[bit]?.plus(1)!! else 1
-
-            }
+            val tempDict = numberOfZeroAndOneForBit(diagnosticReport, i)
             gammaRate += if (tempDict[0]!! > tempDict[1]!!) 0 else 1
             epsilonRate += if (tempDict[0]!! > tempDict[1]!!) 1 else 0
         }
 
         return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2)
     }
+
+    fun diagnosticPartTwo(diagnosticReport: List<String>): Int {
+        var oxygenGeneratorRating = diagnosticReport
+        var co2ScrubberRating = diagnosticReport
+        for (bitIndex in 0 until diagnosticReport[0].length) {
+
+            var tempDict = numberOfZeroAndOneForBit(oxygenGeneratorRating, bitIndex)
+            oxygenGeneratorRating = oxygenGeneratorRating(tempDict, oxygenGeneratorRating, bitIndex)
+
+            tempDict = numberOfZeroAndOneForBit(co2ScrubberRating, bitIndex)
+            co2ScrubberRating = co2ScrubberRating(tempDict, co2ScrubberRating, bitIndex)
+        }
+        return parseInt(oxygenGeneratorRating[0], 2) * parseInt(co2ScrubberRating[0], 2)
+    }
+
+    private fun numberOfZeroAndOneForBit(diagnosticReport: List<String>, bitIndex: Int): HashMap<Int, Int> {
+        val tempDict = hashMapOf<Int, Int>()
+        for (elem in diagnosticReport) {
+            val bit = getNumericValue(elem.toCharArray()[bitIndex])
+            tempDict[bit] = if (tempDict.containsKey(bit)) tempDict[bit]?.plus(1)!! else 1
+        }
+        return tempDict
+    }
+
+    private fun oxygenGeneratorRating(tempDict: HashMap<Int, Int>, oxygenGeneratorRating: List<String>, bitIndex: Int): List<String> {
+        if (tempDict.size <= 1) {
+            return oxygenGeneratorRating
+        }
+        if (tempDict[0]!! > tempDict[1]!!) {
+            return oxygenGeneratorRating.filter { x -> getNumericValue(x[bitIndex]) != 1 }
+        }
+        return oxygenGeneratorRating.filter { x -> getNumericValue(x[bitIndex]) != 0 }
+    }
+
+    private fun co2ScrubberRating(tempDict: HashMap<Int, Int>, co2ScrubberRating: List<String>, i: Int): List<String> {
+        if (tempDict.size <= 1) {
+            return co2ScrubberRating
+        }
+        if (tempDict[0]!! > tempDict[1]!!) {
+            return co2ScrubberRating.filter { x -> getNumericValue(x[i]) != 0 }
+        }
+        return co2ScrubberRating.filter { x -> getNumericValue(x[i]) != 1 }
+    }
+
 }
--- src/test/kotlin/Day3BinaryDiagnosticTest.kt
+++ src/test/kotlin/Day3BinaryDiagnosticTest.kt
@@ -32,6 +32,34 @@ class Day3BinaryDiagnosticTest {
         Assertions.assertEquals(3277364, result)
     }
 
+    @Test
+    fun checkNumberOfTimesDepthMeasurementIncreases_PartTwo() {
+        val diagnosticReport = listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        )
+        val result = Day3BinaryDiagnostic().diagnosticPartTwo(diagnosticReport)
+        Assertions.assertEquals(230, result)
+    }
+
+    @Test
+    fun checkNumberOfTimesDepthMeasurementIncreases_Answer_PartTwo() {
+        val diagnosticReport = retrieveInput()
+
+        val result = Day3BinaryDiagnostic().diagnosticPartTwo(diagnosticReport)
+        Assertions.assertEquals(5736383, result)
+    }
+
     private fun retrieveInput(): List<String> {
         val rows = Day1SonarSweepTest::class.java.getResource("/day3.txt").readText()
         return rows.split("\n").filter { x -> x.isNotBlank() }
